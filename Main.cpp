c5ffaf8f3c03e9b1de92a9148cd938045c4a571b
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-01-14 20:44:08 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,11 @@
 using namespace std;
 
 int main() {
-	array<char, 27> arr{ ' ', 'a', 'b', 'c', 'd', 'e',
-  'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-  'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-  
+	array<char, 27> arr{ ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	char letter = ' ';
 	int sum = 0;
 	cout << "Введiть слово: \n";
-	while (letter != '-') {
+	while (letter != '-') { // Знак стоп
 		cin >> letter;
 		for (int i = 0; i < arr.size(); i++) {
 			if (letter == arr[i]) {
@@ -20,6 +17,6 @@ int main() {
 			}
 		}
 	}
-	cout << "\n---------------\n";
+	cout << "\n----------------------\n";
 	cout << sum;
 }
